6b762fbbe089306d95a6b5ea6a2bdc8dd0945a79
Babis Chalios <bchalios@amazon.es>
2023-07-30 16:39:26 +0000

    Initial configuration
    Some defaults and using gruvbox as colorscheme
    
    Signed-off-by: Babis Chalios <bchalios@amazon.es>

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "gruvbox",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
@@ -33,15 +33,16 @@ return {
       format_on_save = {
         enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
-          -- "go",
+          "rust", "python", "lua"
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
-          -- "python",
+          "C",
+          "C++",
         },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
-        -- "lua_ls",
+        "lua_ls",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
@@ -51,6 +52,7 @@ return {
     -- enable servers that you already have installed without mason
     servers = {
       -- "pyright"
+      "rust_analyzer",
     },
   },
 
